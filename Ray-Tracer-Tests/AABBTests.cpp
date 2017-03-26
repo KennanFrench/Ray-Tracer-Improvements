8b4958655d6ef3bcfddb15171534d2163e04115c
Kennan T French <frenchkt@rose-hulman.edu>
2017-03-26 14:39:27 -0400

    Add tests for AABB.intersect()

--- Ray-Tracer-Tests/AABBTests.cpp
+++ Ray-Tracer-Tests/AABBTests.cpp
@@ -43,5 +43,37 @@ namespace RayTracerTests
 			Assert::AreEqual(v1[2], aabb2.getMax()[2]);
 		}
 
+		TEST_METHOD(AABBIntersect)
+		{
+			Vector3 v1 = Vector3(1.0f, 2.0f, 3.0f);
+			Vector3 v2 = Vector3(4.0f, 5.0f, 6.0f);
+			Vector3 v3 = Vector3(7.0f, 8.0f, 9.0f);
+			Triangle t = Triangle(v1, v2, v3, 0, Material(1.0f, 2.0f, 3.0f, 0.0f, 0.0f));
+
+			vector<Primative*> primList1 = vector<Primative*>();
+			primList1.push_back(&t);
+			AABB aabb1 = AABB(primList1);
+
+			Vector3 hpNorm = Vector3(0.0f, 0.0f, 0.0f);
+			Hitpoint hp = Hitpoint(0, -1, hpNorm);
+			Vector3 origin = Vector3(0.0f, 0.0f, 0.0f);
+
+			Vector3 hitRayDir = Vector3(5.0f, 5.0f, 5.0f);
+			Ray hitRay = Ray(origin, hitRayDir);
+			Assert::IsTrue(aabb1.intersect(hitRay, hp));
+
+			Vector3 missRay1Dir = Vector3(1.0f, 1.0f, 10.0f);
+			Ray missRay1 = Ray(origin, missRay1Dir);
+			hp = Hitpoint(0, -1, hpNorm);
+			hpNorm = Vector3(0.0f, 0.0f, 0.0f);
+			Assert::IsFalse(aabb1.intersect(missRay1, hp));
+
+			Vector3 missRay2Dir = Vector3(-1.0f, -1.0f, -1.0f);
+			Ray missRay2 = Ray(origin, missRay2Dir);
+			hp = Hitpoint(0, -1, hpNorm);
+			hpNorm = Vector3(0.0f, 0.0f, 0.0f);
+			Assert::IsFalse(aabb1.intersect(missRay2, hp));
+		}
+
 	};
 }

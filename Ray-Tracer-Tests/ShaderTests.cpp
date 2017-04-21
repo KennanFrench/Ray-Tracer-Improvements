676dd627f5c5132214f7174e74db450039a3c4ad
Yujia Wu <wuy5@rose-hulman.edu>
2017-04-21 15:56:47 -0400

    All tests passed in ShaderTest

--- Ray-Tracer-Tests/ShaderTests.cpp
+++ Ray-Tracer-Tests/ShaderTests.cpp
@@ -19,13 +19,18 @@ namespace RayGeneratorTests
 			Vector3 v1 = Vector3(1.0f, 2.0f, 3.0f);
 			Material mat = Material(v1, v1, v1, 2.0f, 3.0f);
 			Light light = Light(mat, v1);
-			Hitpoint hp = Hitpoint(1.0f, 1, v1);
 
 			Assert::IsTrue(Vector3(196, 784, 1764) == s.calculateSpecular(mat, light, v1, v1));
 		}
 
 		TEST_METHOD(testCalculateDiffuse) {
+			Shader s = Shader();
+			Vector3 v1 = Vector3(1.0f, 2.0f, 3.0f);
+			Material mat = Material(v1, v1, v1, 2.0f, 3.0f);
+			Light light = Light(mat, v1);
+			Hitpoint hp = Hitpoint(1.0f, 1, v1);
 
+			Assert::IsTrue(Vector3(14, 56, 126) == s.calculateDiffuse(mat, light, v1, hp, v1, v1));
 		}
 	};
 }

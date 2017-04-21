392ddca12fedd4d3bcc3c0e4dfc097dcd96fd8fc
Kennan T French <frenchkt@rose-hulman.edu>
2017-04-21 14:35:14 -0400

    Add blank RayGenerator test class.

---
+++ Ray-Tracer-Tests/ShaderTests.cpp
@@ -0,0 +1,14 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace RayGeneratorTests
+{
+	TEST_CLASS(RayGeneratorTest) {
+	public:
+
+		TEST_METHOD(GetPixelInWorldCoordinates) {
+		}
+	};
+}
